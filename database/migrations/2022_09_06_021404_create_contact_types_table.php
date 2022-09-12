2ca66fcd791c98698e2b9df7813a231cac7b166b
Yosila90 <yossyanugrahenie@students.unnes.ac.id>
2022-09-12 10:53:19 +0700

    SoftDelete ContactType

--- database/migrations/2022_09_06_021404_create_contact_types_table.php
+++ database/migrations/2022_09_06_021404_create_contact_types_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->enum('status',["active", "nonactive"])->nullable();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
