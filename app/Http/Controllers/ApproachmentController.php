87522a6057ddc29350bacd603fc137acbca488ce
ahmad-auni <aramadhan395@students.unnes.ac.id>
2022-09-19 10:07:42 +0700

    lanjut crud approachment
    many-to-many (activity) belum siap

--- app/Http/Controllers/ApproachmentController.php
+++ app/Http/Controllers/ApproachmentController.php
@@ -7,6 +7,7 @@ use App\Models\Status;
 use App\Models\Contact;
 use App\Models\Activity;
 use App\Models\Approachment;
+use App\Models\Fppp;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
@@ -32,31 +33,64 @@ class ApproachmentController extends Controller
     {
         $validation = Validator::make($request->all(), [
             'contact_id' => 'nullable',
+            'status_id' => 'nullable',
             'activity_id' => 'nullable',
             'date' => 'nullable',
             'note' => 'nullable',
         ]);
 
-        try {
-            $approachment = Approachment::create($validation->validated());
-            $approachment->status()->sync($request->status);
-        } catch (Exception $e) {
-            return back()->with('error', $e->getMessage());
-        }  
-        
-        
+        if($validation->fails())
+        {
+            return back()->withErrors($validation)->with('error', 'Approachment gagal dibuat!')->withInput();
+        }
+        else
+        {
+            $create = Approachment::create($request->all());
+            
+            if($create)
+            {
+                return redirect()->route('approachments.index')->with('success', 'Approachment berhasil dibuat!');
+            }
+            else
+            {
+                return redirect()->route('approachments.create')->with('error', 'Approachment gagal dibuat!');
+            }
+        }
 
-        return redirect()->route('approachments.index');
+        // $validation = Validator::make($request->all(), [
+        //     'contact_id' => 'nullable',
+        //     'status_id' => 'nullable',
+        //     'activity_id' => 'nullable',
+        //     'date' => 'nullable',
+        //     'note' => 'nullable',
+        // ]);
+
+        // try {
+        //     $approachment = Approachment::create($validation->validated());
+        //     $approachment->status()->sync($request->status);
+        // } catch (Exception $e) {
+        //     return back()->with('error', $e->getMessage());
+        // }  
+        
+        // return redirect()->route('approachments.index');
     }
 
-    public function edit(Approachment $approachment)
+    public function edit($id)
     {      
-        return view("approachments.edit", compact("approachment"));
+        $approachment = Approachment::findOrFail($id);
+        $contacts = Contact::get();
+        $statuses = Status::where("model", "=", "approachment")->get();
+
+        return view("approachments.edit", compact("approachment", "contacts", "statuses"));
     }
 
-    public function update(Request $request, Approachment $approachment)
+    public function update(Request $request, $id)
     {
+        $approachment = Approachment::findOrFail($id);
+
         $update = $approachment ->update([
+            'contact_id' => $request->contact_id ?? $approachment->contact_id,
+            'status_id'=> $request->status_id ?? $approachment->status_id,
             "date"=>$request->date ?? $approachment->date,
             "note"=>$request->note ?? $approachment->note,
         ]);
