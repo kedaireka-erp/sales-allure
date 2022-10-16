dec9d5ecb742bbb2ceeec1a6cf6ab8a40f07f2fb
ab-azmi <azmi1752@students.unnes.ac.id>
2022-10-17 01:37:29 +0700

    minimize app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -25,19 +25,19 @@ import "@left4code/tw-starter/dist/js/tab";
  | Import 3rd party library JS files.
  |
  */
-import "./chart";
+// import "./chart";
 import "./dashboard-chart";
-import "./highlight";
+// import "./highlight";
 import "./lucide";
 import "./tiny-slider";
-import "./tippy";
-import "./datepicker";
+// import "./tippy";
+// import "./datepicker";
 import "./tom-select";
-import "./dropzone";
-import "./validation";
-import "./zoom";
-import "./notification";
-import "./tabulator";
+// import "./dropzone";
+// import "./validation";
+// import "./zoom";
+// import "./notification";
+// import "./tabulator";
 // import "./calendar";
 
 /*
@@ -51,16 +51,16 @@ import "./tabulator";
 // import "./maps";
 
 // import "./chat";
-import "./show-modal";
+// import "./show-modal";
 // import "./show-slide-over";
-import "./show-dropdown";
+// import "./show-dropdown";
 // import "./search";
-import "./copy-code";
-import "./show-code";
+// import "./copy-code";
+// import "./show-code";
 import "./side-menu";
 import "./mobile-menu";
 import "./side-menu-tooltip";
 import "./dark-mode-switcher";
 import "./fppp-form";
-import "./quotation-form";
+// import "./quotation-form";
 import "./filepond";
--- webpack.mix.js
+++ webpack.mix.js
@@ -13,10 +13,10 @@ const mix = require("laravel-mix");
 
 mix.js("resources/js/app.js", "public/dist/js")
     .js("resources/js/ckeditor-classic.js", "public/dist/js")
-    .js("resources/js/ckeditor-inline.js", "public/dist/js")
-    .js("resources/js/ckeditor-balloon.js", "public/dist/js")
-    .js("resources/js/ckeditor-balloon-block.js", "public/dist/js")
-    .js("resources/js/ckeditor-document.js", "public/dist/js")
+    // .js("resources/js/ckeditor-inline.js", "public/dist/js")
+    // .js("resources/js/ckeditor-balloon.js", "public/dist/js")
+    // .js("resources/js/ckeditor-balloon-block.js", "public/dist/js")
+    // .js("resources/js/ckeditor-document.js", "public/dist/js")
     .css("public/dist/css/_app.css", "public/dist/css/app.css")
     .options({
         processCssUrls: false,
