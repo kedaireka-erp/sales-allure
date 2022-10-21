f4ce5ba1a563d840d857ee040268012a68f74bce
ab-azmi <azmi1752@students.unnes.ac.id>
2022-10-21 16:00:41 +0700

    add role & permission

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,9 +2,10 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Seeder;
-use Illuminate\Support\Str;
 use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class UserSeeder extends Seeder
 {
@@ -16,17 +17,31 @@ class UserSeeder extends Seeder
     public function run()
     {
         // Default credentials
-        \App\Models\User::insert([
-            [ 
-                'name' => 'Left4code',
-                'email' => 'midone@left4code.com',
-                'email_verified_at' => now(),
-                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-                'gender' => 'male',
-                'active' => 1,
-                'remember_token' => Str::random(10)
-            ]
+
+        Role::firstOrCreate(['name' => 'Admin']);
+        Role::firstOrCreate(['name' => 'Sales']);
+
+        User::create([
+
+            'name' => 'Left4code',
+            'email' => 'midone@left4code.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'gender' => 'male',
+            'active' => 1,
+            'remember_token' => Str::random(10)
+
         ]);
+
+        User::create([
+            'name' => 'Samantha',
+            'email' => 'sales@sales.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'gender' => 'female',
+            'active' => 1,
+            'remember_token' => Str::random(10)
+        ])->assignRole('Sales');
 
         // Fake users
         User::factory()->times(9)->create();
